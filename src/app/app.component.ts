a8e06da2d3fa129c64fc6c35e88e65023ef31160
cloudsony999 <cloudsony999@gmail.com>
2022-04-28 12:29:33 +0530

    valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormControl,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'validation';
+export class AppComponent implements OnInit {
+  title = 'validation APP';
+
+  form=new FormGroup(
+    {
+      uname:new FormControl('',[Validators.required,Validators.minLength(3)]),
+      email:new FormControl('',[Validators.required,Validators.email
+      ]),
+      body:new FormControl('',[Validators.required,Validators.maxLength(5)])
+    });
+
+    get f(){
+      return this.form.controls;
+    }
+
+    submit(){
+      console.log(this.form.value);
+    }
+ 
+  ngOnInit(): void {
+    
+  }
+  
 }
